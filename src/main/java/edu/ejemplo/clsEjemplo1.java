8ee39e747e598961ef2b857fb8a900d268e15c7e
Omar Godoy <sgodoym4@miumg.edu.gt>
2023-07-22 12:55:19 -0600

    Initial commit

---
+++ src/main/java/edu/ejemplo/clsEjemplo1.java
@@ -0,0 +1,13 @@
+package edu.ejemplo;
+
+public class clsEjemplo1
+{
+    public void calculo(int a, int b){
+        int resultado;
+
+        resultado =(a+b)*(a-b);
+
+        System.out.println("la respuesta es: "+ resultado);
+    }
+
+}
